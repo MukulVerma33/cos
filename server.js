6ec16aa9b36fb1dc124ce4de5ddb2af6f930ed0e
Anshuman239 <54177794+Anshuman239@users.noreply.github.com>
2021-07-28 17:59:14 +0530

    folder & file structure change for ejs and delete old html files

--- server.js
+++ server.js
@@ -6,8 +6,26 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("static"))
 
+app.set("view engine", "ejs")
+
 app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/index.html")
+    res.render("index")
+})
+
+app.get("/bookbank", (req, res)=>{
+    res.render("bookbank")
+})
+
+app.get("/activities", (req, res)=>{
+    res.render("activities")
+})
+
+app.get("/videos", (req, res)=>{
+    res.render("videos")
+})
+
+app.get("/articles", (req, res)=>{
+    res.render("articles")
 })
 
 app.listen(3000, (req, res)=>{
